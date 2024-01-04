9f32ac1e348ad0bc7946ac906c0284fb29f702b4
tukuanchung <tukuanchung@gmail.com>
2024-01-04 11:29:06 +0800

    正確載入js

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ng-router';
+
+  ngOnInit(): void {
+    const chartAreaScript = document.createElement('script');
+    chartAreaScript.src = "assets/js/demo/chart-area-demo.js";
+    document.body.appendChild(chartAreaScript);
+
+    const chartPieScript = document.createElement('script');
+    chartAreaScript.src = "assets/js/demo/chart-pie-demo.js";
+    document.body.appendChild(chartPieScript);
+  }
 }
