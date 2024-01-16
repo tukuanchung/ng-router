253e4a53a58f2a43e9ea10b7d26641b1b993182d
tukuanchung <tukuanchung@gmail.com>
2024-01-16 09:56:38 +0800

    示範用 .subscribe 訂閱結果，用 Subscription 取消訂閱非同步事件

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,26 +1,37 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { DataService } from 'src/app/data.service';
-import { News } from 'src/app/models/news';
+import { News, NewsElement } from 'src/app/models/news';
 
 @Component({
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+
   router = inject(Router);
   route = inject(ActivatedRoute);
-  data = inject(DataService)
+  data = inject(DataService);
   // news$: any;
-  news$!:Observable<News>
+  news$!:Observable<News>;
+
+  news : NewsElement[] = [];
+  newsSubscription!: Subscription;
 
   ngOnInit(): void {
     const chartAreaScript = document.createElement('script');
     chartAreaScript.src = "assets/js/demo/chart-area-demo.js";
     document.body.appendChild(chartAreaScript);
     this.data.firstClick();
-    this.news$ = this.data.loadNews();
+    // this.news$ = this.data.loadNews(); 推薦寫法
+
+    // 替代寫法，當資料在ts還是會改變就要用下面的寫法
+    this.newsSubscription = this.data.loadNews().subscribe((data)=>{
+      this.news = data.news;
+    });
+
+
     const chartPieScript = document.createElement('script');
     chartAreaScript.src = "assets/js/demo/chart-pie-demo.js";
     document.body.appendChild(chartPieScript);
@@ -29,6 +40,9 @@ export class DashboardComponent implements OnInit {
       console.log(data['pageTitle']);
     });
 
+  }
+  ngOnDestroy(): void {
+    this.newsSubscription.unsubscribe();
   }
   gotoPage(pageName:string, name: string, id: number): void{
     this.router.navigate([`${pageName}/${name}`],{
